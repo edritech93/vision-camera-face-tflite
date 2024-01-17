7228ff1e4c47b5e887cda08ae549ce73d73d9f99
Yudi Alviska <yudi.a@weefer.co.id>
2024-01-17 14:54:37 +0700

    fix: integrate plugin android

--- android/src/main/java/com/visioncamerafacetflite/VisionCameraFaceTflitePackage.kt
+++ android/src/main/java/com/visioncamerafacetflite/VisionCameraFaceTflitePackage.kt
@@ -5,7 +5,6 @@ import com.facebook.react.bridge.NativeModule
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.uimanager.ViewManager
 import com.mrousavy.camera.frameprocessor.FrameProcessorPluginRegistry
-import com.visioncamerafacetflite.visioncamerafacetfliteplugin.VisionCameraFaceTflitePluginPlugin
 
 class VisionCameraFaceTflitePackage : ReactPackage {
   companion object {
